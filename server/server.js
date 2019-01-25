4d83428d52cd6305fcb410ade20632b141b6794d
Ryan Whitecar <ryanwhitecar@gmail.com>
2019-01-24 18:39:07 -0700

    changes to server file

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 // const massive = require('massive')
 const bodyParser = require('body-parser')
 const path = require('path');
-// require('dotenv').config()
+require('dotenv').config()
 
 const app = express();
 const port = process.env.SERVER_PORT;
